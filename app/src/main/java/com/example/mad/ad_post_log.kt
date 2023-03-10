bf70b4767806517694b434ca78998d1a126f5e13
IT21309038 <it21309038@my.sliit.lk>
2023-03-10 23:22:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad/ad_post_log.kt
@@ -0,0 +1,11 @@
+package com.example.mad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ad_post_log : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ad_post_log)
+    }
+}
