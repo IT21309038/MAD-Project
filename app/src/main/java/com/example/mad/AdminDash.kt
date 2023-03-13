61db5411198a453b8b5f9eda2f5b725115118c43
IT21309038 <it21309038@my.sliit.lk>
2023-03-13 19:19:59 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad/AdminDash.kt
@@ -0,0 +1,11 @@
+package com.example.mad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminDash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_dash)
+    }
+}
