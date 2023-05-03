c3f5befb103b538b80a7871765c107a97f988e9a
IT21309038 <it21309038@my.sliit.lk>
2023-05-03 14:27:28 +0530

    backend adpost insert push

--- app/src/main/java/com/example/mad/AdAd.kt
+++ app/src/main/java/com/example/mad/AdAd.kt
@@ -2,10 +2,90 @@ package com.example.mad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class AdAd : AppCompatActivity() {
+    // Define the Firebase database reference
+    private lateinit var database: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ad_ad)
+
+        // Get a reference to the Firebase database
+        database = FirebaseDatabase.getInstance().reference
+
+        // Get references to the UI elements
+        val comNameEditText = findViewById<EditText>(R.id.ComName)
+        val positionEditText = findViewById<EditText>(R.id.Position)
+        val conMailEditText = findViewById<EditText>(R.id.ConMail)
+        val jobDesEditText = findViewById<EditText>(R.id.JobDes)
+        val m1RadioButton = findViewById<RadioButton>(R.id.M1)
+        val m2RadioButton = findViewById<RadioButton>(R.id.M2)
+        val m3RadioButton = findViewById<RadioButton>(R.id.M3)
+        val durationRadioGroup = findViewById<RadioGroup>(R.id.Due)
+        val btn3 = findViewById<Button>(R.id.Btn3)
+
+        // Set a click listener on the button
+        btn3.setOnClickListener {
+            // Get the values of the UI elements
+            val comName = comNameEditText.text.toString()
+            val position = positionEditText.text.toString()
+            val conMail = conMailEditText.text.toString()
+            val jobDes = jobDesEditText.text.toString()
+            val duration = getDurationString(durationRadioGroup.checkedRadioButtonId)
+
+            // Create a Job object
+            val job = Job(comName, position, conMail, jobDes, duration)
+
+            // Save the job object to the database
+            database.child("jobs").push().setValue(job.toMap())
+                .addOnSuccessListener {
+                    // Show a toast message when the data is successfully added
+                    Toast.makeText(this, "Data added successfully", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    // Show an error message when the data could not be added
+                    Toast.makeText(this, "Error: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+
+            // Clear the UI elements
+            comNameEditText.setText("")
+            positionEditText.setText("")
+            conMailEditText.setText("")
+            jobDesEditText.setText("")
+            durationRadioGroup.clearCheck()
+        }
+    }
+
+    private fun getDurationString(checkedRadioButtonId: Int): String {
+        return when (checkedRadioButtonId) {
+            R.id.M1 -> "1$-1 month"
+            R.id.M2 -> "2$-2 months"
+            R.id.M3 -> "3$-3 months"
+            else -> ""
+        }
     }
 }
+
+// Define a data class to represent a job
+data class Job(
+    val comName: String = "",
+    val position: String = "",
+    val conMail: String = "",
+    val jobDes: String = "",
+    val duration: String = ""
+) {
+    fun toMap(): Map<String, Any?> {
+        return mapOf(
+            "comName" to comName,
+            "position" to position,
+            "conMail" to conMail,
+            "jobDes" to jobDes,
+            "duration" to duration
+        )
+    }
+}
+
